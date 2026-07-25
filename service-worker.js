283a87289b0a5f9d5b657cfac64e0b874e1c536b
DmytroK1994 <dmytrok1994@icloud.com>
2026-07-25 05:53:51 +0300

    1

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "oblik-zalyshkiv-v2-64";
+const CACHE_NAME = "oblik-zalyshkiv-v2-67";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./manifest.json",
+  "./manifest.json?v=267",
   "./icon.svg?v=264",
   "./icon-192.png?v=264",
   "./icon-512.png?v=264",
