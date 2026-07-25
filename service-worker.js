6794cac4acbb6d5cf0795808487599a8199ffe1f
DmytroK1994 <dmytrok1994@icloud.com>
2026-07-26 01:59:40 +0300

    1

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "oblik-v3-03";
+const CACHE_NAME = "oblik-v3-04";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./manifest.json?v=303",
+  "./manifest.json?v=304",
   "./icon-192.png?v=303",
   "./icon-512.png?v=303",
   "./assets/qrcode-generator.js?v=271",
