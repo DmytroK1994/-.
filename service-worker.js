315aa7e0fa40b6c6fc39f0535d53a535874c3c70
DmytroK1994 <dmytrok1994@icloud.com>
2026-08-03 00:51:20 +0300

    1

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = "oblik-v4-30";
+const CACHE_NAME = "oblik-v4-31";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./manifest.json?v=430",
+  "./manifest.json?v=431",
   "./icon-192.png?v=303",
   "./icon-512.png?v=303",
   "./assets/qrcode-generator.js?v=368",
   "./assets/jsQR.js?v=368",
-  "./assets/decimen-optical-transfer.js?v=430",
-  "./assets/ForkliftGame2D.js?v=430",
-  "./firebase-config.js?v=430",
+  "./assets/decimen-optical-transfer.js?v=431",
+  "./assets/ForkliftGame2D.js?v=431",
+  "./firebase-config.js?v=431",
   "./social-preview.png?v=302",
   "./banner.png?v=302"
 ];
